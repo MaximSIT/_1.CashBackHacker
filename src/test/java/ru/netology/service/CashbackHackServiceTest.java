98c0bb1279cc3983ac363e8e25507e1a2b39434a
Maxim <max_dancer94@mail.ru>
2023-03-26 22:03:45 +0500

    JUnit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,16 +1,25 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
-    public void remainTest() {
+    public void remainTestOne() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1100;
         int expected = 900;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainTestBorder() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 }
